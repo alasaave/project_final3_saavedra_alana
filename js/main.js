29eaacf092d09829c083c4896867343e30622441
Alana Saavedra <alasaave@gmail.com>
2018-04-13 22:25:49 -0500

    add custom JS to add content (on hours section)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
-    //insert Facebook message into header
-    $('<p id="sideNote">Check out our Facebook page for last minute changes!</p>').insertBefore('#hours');
+    //insert Facebook message into header using JS
+    //Using jQuery: $('<p id="sideNote">Check out our Facebook page for last minute changes!</p>').insertBefore('#hours');
+
+    var checkMe = document.createElement('p');
+    var hoursFirst = document.getElementById('hours');
+
+    checkMe.id = 'sideNote';
+    checkMe.innerText = 'Check out our Facebook page for last minute changes!';
+    hoursFirst.insertBefore(checkMe, hoursFirst.childNodes[0]);
+
 });
