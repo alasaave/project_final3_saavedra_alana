f920758fad79b10b256eeaa3a004fbb220bba840
Alana Saavedra <alasaave@gmail.com>
2018-04-13 21:05:20 -0500

    finish styling for column 1 (header, nav, hours)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 $(document).ready(function () {
     //insert Facebook message into header
-    $('<p>Check out our Facebook page for last minute changes!</p>').insertBefore('#hours');
+    $('<p id="sideNote">Check out our Facebook page for last minute changes!</p>').insertBefore('#hours');
 });
