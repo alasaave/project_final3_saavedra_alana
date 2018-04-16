835b819aade1728e26b63ce9fd3852bef972fff9
Alana Saavedra <alasaave@gmail.com>
2018-04-16 00:14:37 -0500

    add Facebook plugin

--- js/main.js
+++ js/main.js
@@ -11,4 +11,24 @@ $(document).ready(function () {
     //Call Hamburger Menu
     $.fatNav();
 
+    //social Feed
+    //Facebook sdk
+    window.fbAsyncInit = function () {
+        FB.init({
+            appId: '',
+            autoLogAppEvents: true,
+            xfbml: true,
+            version: 'v2.12'
+        });
+    };
+
+    (function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {
+            return;
+        }
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
 });
