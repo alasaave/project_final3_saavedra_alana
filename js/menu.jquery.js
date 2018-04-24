34e4aebe99227332d0b89cfcfb2f9af273eb3d2d
Alana Saavedra <alasaave@gmail.com>
2018-04-23 20:37:36 -0500

    reduce size of text and width of body on mobile

--- js/menu.jquery.js
+++ js/menu.jquery.js
@@ -1,40 +1,40 @@
-(function($){
-	$.fn.menu = function(opts){
-		var $this = this,
-				wi = $(window).width(),
-				selector = this.find('li'),
-				t,
-				subMenu,
-				animateMenu = function(elem, duration, callback){
-					$(elem).prev('a').toggleClass('hover');
-					if (wi >= 960) {
-						$(elem).stop().slideToggle(duration, callback);
-						$this.removeClass('open');
-					}
-				},
-				defaults = {
-					menuSpeed: 50,
-					callback: function(){}
-				},
-				options = $.extend({}, defaults, opts);
-		$(window).resize(function(){
-			wi = $(this).width();
-			if (wi >= 960) {
-				$this.removeClass('open');
-			}
-		});
-		$('.re-order').on('click', function(){
-			$this.toggleClass('open');
-		});
-		return selector.each(function(){
-			t = $(this);
-			t.find('ul').parent('li').addClass('parent');
-			if ($(this).hasClass('parent')){
-				$(this).hover(function(){
-					subMenu = $(this).children('ul');
-					animateMenu(subMenu, options.menuSpeed, options.callback);
-				});
-			}
-		});
-	};
+(function ($) {
+    $.fn.menu = function (opts) {
+        var $this = this,
+            wi = $(window).width(),
+            selector = this.find('li'),
+            t,
+            subMenu,
+            animateMenu = function (elem, duration, callback) {
+                $(elem).prev('a').toggleClass('hover');
+                if (wi >= 600) {
+                    $(elem).stop().slideToggle(duration, callback);
+                    $this.removeClass('open');
+                }
+            },
+            defaults = {
+                menuSpeed: 50,
+                callback: function () {}
+            },
+            options = $.extend({}, defaults, opts);
+        $(window).resize(function () {
+            wi = $(this).width();
+            if (wi >= 600) {
+                $this.removeClass('open');
+            }
+        });
+        $('.re-order').on('click', function () {
+            $this.toggleClass('open');
+        });
+        return selector.each(function () {
+            t = $(this);
+            t.find('ul').parent('li').addClass('parent');
+            if ($(this).hasClass('parent')) {
+                $(this).hover(function () {
+                    subMenu = $(this).children('ul');
+                    animateMenu(subMenu, options.menuSpeed, options.callback);
+                });
+            }
+        });
+    };
 })(jQuery);
